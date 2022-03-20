8ab8a1cbc5cdfb967db60b02b98a179f04f0c1af
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2022-03-20 10:03:18 -0300

    estruturado exemplo

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$funcionario = new Funcionario();
+$funcionario->setNome('Fulano');
+$funcionario->setSalarioBase(1000);
+$funcionario->setHorasExtras(10);
+$funcionario->setValorDaHora(20);
+
+echo $funcionario->getSalarioEfetivo();
+
